cbf827780c15c441a1beb287d0ef2f41cb8b436a
Clive Jeffery <clive@rosedene.com>
2023-12-29 16:25:50 +0000

    Completed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,18 @@
 import { defineConfig } from 'astro/config';
-
+import preact from "@astrojs/preact";
 const setLayout = () => {
-    return function (_, file) {
-      file.data.astro.frontmatter.layout =
-        file.data.astro.frontmatter.layout || "/src/layouts/MarkdownPostLayout.astro";
-    };
+  return function (_, file) {
+    file.data.astro.frontmatter.layout = file.data.astro.frontmatter.layout || "/src/layouts/MarkdownPostLayout.astro";
   };
+};
 
-  // https://astro.build/config
-  export default defineConfig({
-    markdown: {
-      remarkPlugins: [setLayout],
-    },
-    // ...
-  });
+
+// https://astro.build/config
+export default defineConfig({
+  markdown: {
+    remarkPlugins: [setLayout]
+  }
+  // ...
+  ,
+  integrations: [preact()]
+});
